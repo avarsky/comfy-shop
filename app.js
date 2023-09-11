e8598793a1058589e29d93f56e2399c2da88d30b
mustafa sungur ardıç <sungur@mustafas-MacBook-Pro.local>
2023-09-11 07:15:13 +0300

    before js

---
+++ app.js
@@ -0,0 +1,58 @@
+//variables
+
+const cartBtn = document.querySelector(".cart-btn"); 
+const closeCartBtn = document. querySelector (" ‚close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDom = document.querySelector(".products-center");
+
+
+let cart = []
+
+// getting the products
+
+class Products{
+async getProducts(){
+try {
+    let result = await fetch("products.json");
+    let data = await result.json();
+
+    let products = data.items;
+     products = products.map(item =>{
+        const {title,price} = item.fields;
+        const {id} = item.sys;
+        const image = item.fields.image.fields.file.url;
+        return{ title, price, id, image}
+     })
+    return products;
+} catch (error) {
+    console.log(error);
+}
+}
+}
+
+//display Products
+class UI{
+displayProducts(products){
+
+}
+}
+
+//local storage
+
+class Storage {
+
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    const ui = new UI();
+    const products = new Products();
+
+    // get all Products
+    products.getProducts().then(products => ui.displayProducts
+        (products));
+});
